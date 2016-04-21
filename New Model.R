3d0ab0de31c9c8f91851e9c8dcc78b86095447a7
JHendy <jhendy@vols.utk.edu>
2016-04-21 10:33:29 -0400

    Starting workflow.  I'm just trying to make sure I'm on the right track.  Although I'm not really to sure where to begin.

---
+++ New Model.R
@@ -0,0 +1,28 @@
+#Function will need to:
+#  Input
+#Species names 
+#Multiple binary traits for each species (perennial v annual)
+#Multiple determinant binary traits for each species (flower v non-flower crops)		
+
+#Install appropriate libraries
+#Load appropriate libraries
+#Input data
+
+#Set input parameters
+#Tell function which traits are determinant and which ones are not
+#Return list of determinant traits and non-determinant traits
+
+#Build a tree
+#Or should I just require a premade tree?
+#How should tree be built?
+
+#Use Pagel to map traits on tree
+#Constrain tree root to match wild type
+#Phytools - fitPagel()
+#Use Pagel to 
+#Compare these traits correlated to a binary determinant trait (wild type v domesticate)
+#Do this for multiple determinant traits
+
+
+#End goal of function
+#Tell which traits are correlated with which determinant traits
