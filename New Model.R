b73e8757fa792c211a9cc414767c9bf3ffe13533
JHendy <jhendy@vols.utk.edu>
2016-04-26 15:58:43 -0400

    Updated the workflow of how the function needs to be setup

--- New Model.R
+++ New Model.R
@@ -1,28 +1,40 @@
-#Function will need to:
-#  Input
-#Species names 
-#Multiple binary traits for each species (perennial v annual)
-#Multiple determinant binary traits for each species (flower v non-flower crops)		
-
-#Install appropriate libraries
 #Load appropriate libraries
+library(ape)
+library(geiger)
+library(phytools)
+
 #Input data
+# a "phylo" tree
+# a vector of phenotypic values for multiple binary traits for the species in tree (x)
+# each different trait needs to go into a different column
+# a binary character for the species in the tree that all other traits will be tested against (y)
+# for this example that would be wild v. domesticated
 
-#Set input parameters
-#Tell function which traits are determinant and which ones are not
-#Return list of determinant traits and non-determinant traits
+setwd("~/GitHub/New-Method")
+Trial.data<-read.csv("Trial_data.csv")
 
-#Build a tree
-#Or should I just require a premade tree?
-#How should tree be built?
+# Build a tree first then output tree file with other results
+# Give option to have function build a tree or use a given tree
 
-#Use Pagel to map traits on tree
-#Constrain tree root to match wild type
-#Phytools - fitPagel()
-#Use Pagel to 
-#Compare these traits correlated to a binary determinant trait (wild type v domesticate)
-#Do this for multiple determinant traits
+# have a check function to check and make sure function has everything it needs to run
+
+# make a loop function that runs Pagel for each of the traits (x) given against a single trait (y)
+#   if you have six traits it should run once for each trait
+# Setup a way to constrain the root of the tree to match a particular trait (y)
+#   in this case it would be to constrain tree root to match wild type
+
+# loop fitPagel(tree, x<-1, y)
+# fitPagel (tree, [x<-1+previous line, up to total number of traits, then stop], y)
+
+# Output
 
 
 #End goal of function
-#Tell which traits are correlated with which determinant traits
+#Tell which traits are correlated with which the determinant trait
+
+#fitPagel(tree, x, y, ...)
+# tree - an object of class "phylo"
+# x - a vector of phenotypic values for a binary trait for the species in tree
+# y - a second binary character for the species in tree
+# ... - optional argumett. Currently includes method, which can be set to "ace" to use the ace function in ape for optimization.
+# Or to "fitDiscrete" (if geiger package is installed) to use geiger's firDiscrete for optimization
